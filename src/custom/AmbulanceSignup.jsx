c1e72c5d3e703272c17fccf6a3864d4a1e37d23a
Pavan khalyan <pavankhalyan2004@gmail.com>
2024-06-27 23:43:05 +0530

    signup

---
+++ src/custom/AmbulanceSignup.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AmbulanceSignup = () => {
+  return (
+    <View>
+      <Text>AmbulanceSignup</Text>
+    </View>
+  )
+}
+
+export default AmbulanceSignup
+
+const styles = StyleSheet.create({})
---
+++ src/custom/PoliceSignup.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PoliceSignup = () => {
+  return (
+    <View>
+      <Text>PoliceSignup</Text>
+    </View>
+  )
+}
+
+export default PoliceSignup
+
+const styles = StyleSheet.create({})
